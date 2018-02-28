3d2f0311b1db1652cf6855c0fa86ce295e446c7a
Colin Nico Schmid <mail@mala23.me>
2018-02-28 11:21:58 +0100

    make it count

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
-  state = {
-    count: 0,
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+    }
   }
+
+  add = () => {
+    console.log('click')
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+  
   render() {
     return (
       <section>
         <p>Current count is: {this.state.count}</p>
-        <button>+</button>
+        <button onClick={this.add}>+</button>
       </section>
     )
   }
